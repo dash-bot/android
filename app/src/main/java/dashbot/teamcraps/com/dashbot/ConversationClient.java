da67780adcc8e997c04b3cdfd87b561918dfc0c2
shaebrown <shaeb@uvic.ca>
2018-01-14 02:09:21 -0800

    started enrollment client call

--- app/src/main/java/dashbot/teamcraps/com/dashbot/ConversationClient.java
+++ app/src/main/java/dashbot/teamcraps/com/dashbot/ConversationClient.java
@@ -2,11 +2,14 @@ package dashbot.teamcraps.com.dashbot;
 
 
 import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpMediaType;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpRequestFactory;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.MultipartContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URLEncoder;
 
@@ -15,12 +18,27 @@ import java.net.URLEncoder;
  */
 public class ConversationClient {
     private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
-    private static String url = "https://api2.teamcraps.com/?command=%s";
+    private static String command_url = "https://api2.teamcraps.com/?command=%s";
+    private static String voice_url = "https://api2.teamcraps.com/%s";
     private static HttpRequestFactory requestFactory = HTTP_TRANSPORT.createRequestFactory();
     public static String getReply(String command) throws IOException {
-        String request_url = String.format(url, URLEncoder.encode(command, "UTF-8"));
-        System.out.println(request_url);
+        String request_url = String.format(command_url, URLEncoder.encode(command, "UTF-8"));
         HttpRequest request = requestFactory.buildGetRequest(new GenericUrl(request_url));
         return request.execute().parseAsString();
+    }
+
+    /**
+     *
+     * @param wavFile
+     * @return remaining enrollments
+     * @throws IOException
+     */
+    public static int enroll(File wavFile) throws IOException {
+        String request_url = String.format(voice_url, "enroll");
+        MultipartContent content = new MultipartContent();
+        content.setMediaType(new HttpMediaType("multipart/form-data"));
+
+        //TODO figure out how to add wav file as form-data with key 'file'
+        return 3;
     }
 }
