44cf2a010c51aa0b2b9db6ca7bb133911f9d5425
Paul Sajna <paulsajna@gmail.com>
2018-01-14 02:55:57 -0800

    conversation recyclerview and such

--- app/src/main/java/dashbot/teamcraps/com/dashbot/ConversationActivity.java
+++ app/src/main/java/dashbot/teamcraps/com/dashbot/ConversationActivity.java
@@ -2,12 +2,27 @@ package dashbot.teamcraps.com.dashbot;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class ConversationActivity extends AppCompatActivity {
-
+    private RecyclerView mMessageRecycler;
+    private MessageListAdapter mMessageAdapter;
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_conversation);
+
+        ArrayList<String> messageList = new ArrayList<String>();
+
+        mMessageRecycler = (RecyclerView) findViewById(R.id.reyclerview_message_list);
+        mMessageAdapter = new MessageListAdapter(this, messageList);
+        mMessageRecycler.setLayoutManager(new LinearLayoutManager(this));
+
+        messageList.add("Hello Dash");
+        messageList.add("Hello Paul");
     }
 }
--- app/src/main/java/dashbot/teamcraps/com/dashbot/MessageListAdapter.java
+++ app/src/main/java/dashbot/teamcraps/com/dashbot/MessageListAdapter.java
@@ -2,13 +2,77 @@ package dashbot.teamcraps.com.dashbot;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-//public class MessageListAdapter extends RecyclerView.Adapter {
-//    private Context mContext;
-//    private List<BaseMessage> mMessageList;
-//
-//    public MessageListAdapter(Context context, List<BaseMessage> messageList) {
-//        mContext = context;
-//        mMessageList = messageList;
-//    }
-//}
+import java.util.List;
+
+public class MessageListAdapter extends RecyclerView.Adapter {
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+    private Context mContext;
+    private List<String> mMessageList;
+
+    public MessageListAdapter(Context context, List<String> messageList) {
+        mContext = context;
+        mMessageList = messageList;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mMessageList.size();
+    }
+
+    // Determines the appropriate ViewType according to the sender of the message.
+    @Override
+    public int getItemViewType(int position) {
+     if (position % 2 == 0) {
+            return VIEW_TYPE_MESSAGE_SENT;
+        } else {
+            // If some other user sent the message
+            return VIEW_TYPE_MESSAGE_RECEIVED;
+        }
+    }
+
+    // Inflates the appropriate layout according to the ViewType.
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view;
+
+        if (viewType == VIEW_TYPE_MESSAGE_SENT) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_sent, parent, false);
+            return new MessageHolder(view);
+        } else if (viewType == VIEW_TYPE_MESSAGE_RECEIVED) {
+            view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_message_received, parent, false);
+            return new MessageHolder(view);
+        }
+
+        return null;
+    }
+
+    // Passes the message object to a ViewHolder so that the contents can be bound to UI.
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        String message = mMessageList.get(position);
+        ((MessageHolder) holder).bind(message);
+    }
+
+    private class MessageHolder extends RecyclerView.ViewHolder {
+        TextView messageText, timeText;
+
+        MessageHolder(View itemView) {
+            super(itemView);
+
+            messageText = (TextView) itemView.findViewById(R.id.text_message_body);
+        }
+
+        void bind(String message) {
+            messageText.setText(message);
+        }
+    }
+}
