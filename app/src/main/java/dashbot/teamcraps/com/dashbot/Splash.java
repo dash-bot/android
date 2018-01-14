6e617c61ed7bedabd652c0efb4acc44ab9cd4c9f
rhysdangerlawson <rlawson5775@gmail.com>
2018-01-13 22:54:12 -0800

    Another commit

--- app/src/main/java/dashbot/teamcraps/com/dashbot/Splash.java
+++ app/src/main/java/dashbot/teamcraps/com/dashbot/Splash.java
@@ -1,8 +1,56 @@
 package dashbot.teamcraps.com.dashbot;
 
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
 /**
  * Created by Rhys on 2018-01-13.
  */
 
-public class Splash {
+public class Splash extends AppCompatActivity{
+    public int DELAY = 4000; // Milliseconds
+
+    private AnimationDrawable animationDrawable;
+    private ImageView mProgressBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+
+        // Set-up anything needs to be set up before running MainActivity here.
+        setUpAnimation();
+        setTimer(DELAY);
+    }
+
+    private void setTimer(int time){
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Stop the animation
+                mProgressBar.setVisibility(View.GONE);
+                animationDrawable.stop();
+
+                // Run the next activity
+                startActivity(new Intent(getBaseContext(),MainActivity.class));
+                finish();
+            }
+        }, time);
+    }
+
+    private void setUpAnimation(){
+        mProgressBar.setBackgroundResource(R.drawable.progress);
+        animationDrawable = (AnimationDrawable)mProgressBar.getBackground();
+        mProgressBar.setVisibility(View.VISIBLE);
+        animationDrawable.start();
+    }
 }
