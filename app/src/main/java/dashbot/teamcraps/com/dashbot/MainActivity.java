be2a64f4fdf861aed044f06aa17b2ab6f014ce7a
rlawson5775@gmail.com <rlawson5775@gmail.com>
2018-01-14 10:18:15 -0800

    The following interfaces were implemented: main screen, messaging screen. All of the icons in those interfaces were also added.

--- app/src/main/java/dashbot/teamcraps/com/dashbot/MainActivity.java
+++ app/src/main/java/dashbot/teamcraps/com/dashbot/MainActivity.java
@@ -1,17 +1,47 @@
 package dashbot.teamcraps.com.dashbot;
 
 import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity  {
+
+    private LinearLayout savings;
+    private LinearLayout chequings;
+
+    private HorizontalScrollView horizontalScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        savings = findViewById(R.id.mainPage_savings_ll);
+        chequings = findViewById(R.id.mainPage_chequings_ll);
+
+        horizontalScrollView = findViewById(R.id.mainPage_hsv);
+
+        Toolbar myToolbar = findViewById(R.id.mainPage_toolbar);
+        setSupportActionBar(myToolbar);
+
+        FloatingActionButton fab= findViewById(R.id.mainPage_fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(),ConversationActivity.class));
+            }
+        });
+
+        // Scrollview support
+        savings.getParent().requestChildFocus(savings,savings);
+        chequings.getParent().requestChildFocus(chequings,chequings);
+        horizontalScrollView.fullScroll(HorizontalScrollView.FOCUS_LEFT);
+
         //Intent intent = new Intent(this, ConversationActivity.class);
         //startActivity(intent);
     }
